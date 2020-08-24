28baae41e7adb69d0e7569395ba8c7512c65716c
tavishkar98 <42904824+tavishkar98@users.noreply.github.com>
2020-08-24 11:09:18 +0530

    Done

---
+++ eclipse-workspace/My-Service-Customer/src/main/java/com/example2/training/rest/CustomerController.java
@@ -0,0 +1,38 @@
+package com.example2.training.rest;
+
+import java.util.Optional;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example2.training.model.Customer;
+import com.example2.training.service.CustomerService;
+
+
+
+@RestController
+public class CustomerController {
+
+	private CustomerService customerService;
+
+	public CustomerController(CustomerService customerService) {
+		super();
+		this.customerService = customerService;
+	}
+	
+	@GetMapping("/customers")
+	public ResponseEntity<Iterable<Customer>> getAllCustomer()
+	{
+		return new ResponseEntity<Iterable<Customer>>(customerService.getAllCustomer(),HttpStatus.OK);
+	}
+	@GetMapping("/customers/{customerId}")
+	
+	public ResponseEntity<Optional<Customer>> findCustomerById(@PathVariable("customerId") String customerId)
+	{
+		return ResponseEntity.status(HttpStatus.FOUND).body(customerService.findCustomerById(customerId));
+	}
+}
---
+++ eclipse-workspace/My-Service-Customer/src/main/java/com/example2/training/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.example2.training.service;
+
+import java.util.Optional;
+
+
+import com.example2.training.model.Customer;
+
+
+public interface CustomerService {
+
+	public Iterable<Customer> getAllCustomer();
+	public Optional<Customer> findCustomerById(String customerId);
+}
